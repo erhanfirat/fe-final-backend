d532a926945e5718f37f2b91c02c46be96177ef5
Erhan Fırat <erhanfirat@gmail.com>
2023-10-02 03:04:35 +0300

    generateActivationToken & sendActivationEmail functions added to utils

---
+++ utils/utils.js
@@ -0,0 +1,39 @@
+function sendActivationEmail(email, activationToken) {
+  // Configure Nodemailer transporter with your email service provider's settings
+  const transporter = nodemailer.createTransport({
+    service: "Yandex",
+    auth: {
+      user: "erhan@workintech.com.tr",
+      pass: "Workintech34728",
+    },
+  });
+
+  // Email content
+  const mailOptions = {
+    from: "admin@workintech-fe-ecommerce.com",
+    to: email,
+    subject: "Activate Your Account",
+    text: `Click the following link to activate your account: 
+           http://yourwebsite.com/activate?token=${activationToken}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending activation email:", error);
+    } else {
+      console.log("Activation email sent:", info.response);
+    }
+  });
+}
+
+const { v4: uuidv4 } = require("uuid");
+
+function generateActivationToken() {
+  return uuidv4();
+}
+
+module.exports = {
+  sendActivationEmail,
+  generateActivationToken,
+};
