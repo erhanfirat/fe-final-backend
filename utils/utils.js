c952f6a48bdd9db48fed17f632377de621466267
Erhan Fırat <erhanfirat@gmail.com>
2023-10-03 22:40:28 +0300

    sendEmail func added and db update

--- utils/utils.js
+++ utils/utils.js
@@ -2,21 +2,37 @@ const nodemailer = require("nodemailer");
 
 function sendActivationEmail(email, activationToken) {
   // Configure Nodemailer transporter with your email service provider's settings
+
+  const message = `
+
+Congrats!
+
+Your registration to E-Commerce is succesfull. The only thing you need to do is to click the link below to activate your account and start shopping!
+
+Activate your account: ${process.env.SERVER_URL}/activate?token=${activationToken}
+
+Sincerely
+
+E-Commerce Team
+`;
+  sendEmail(email, "Activate Your E-Commerce Account", message);
+}
+
+const sendEmail = (email, subject, message) => {
   const transporter = nodemailer.createTransport({
     service: "Yandex",
     auth: {
-      user: "erhan@workintech.com.tr",
-      pass: "Workintech34728",
+      user: process.env.ADMIN_MAIL,
+      pass: process.env.ADMIN_PASS,
     },
   });
 
   // Email content
   const mailOptions = {
-    from: "erhan@workintech.com.tr",
+    from: process.env.ADMIN_MAIL,
     to: email,
-    subject: "Activate Your Account",
-    text: `Click the following link to activate your account: 
-           http://yourwebsite.com/activate?token=${activationToken}`,
+    subject: subject,
+    text: message,
   };
 
   // Send the email
@@ -27,7 +43,7 @@ function sendActivationEmail(email, activationToken) {
       console.log("Activation email sent:", info.response);
     }
   });
-}
+};
 
 const { v4: uuidv4 } = require("uuid");
 
@@ -38,4 +54,5 @@ function generateActivationToken() {
 module.exports = {
   sendActivationEmail,
   generateActivationToken,
+  sendEmail,
 };
